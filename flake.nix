345b9ba75804dbdfa69b5b23e4a30be8c795ff9e
Zane Dufour <zane@znd4.dev>
2025-02-24 12:13:12 -0800

    switch to nixos 24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";

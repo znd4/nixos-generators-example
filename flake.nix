c5604b5c132559dd37a1ceb5c462ff22f501569a
Zane Dufour <zane@znd4.dev>
2025-02-24 12:02:27 -0800

    first commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    xc = {
+      url = "github:joerdav/xc";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, nixos-generators, xc, ... }:
+    let
+      pkgsForSystem = system: import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: { xc = xc.packages.${system}.xc; })
+        ];
+      };
+      allVMs = [ "x86_64-linux" "aarch64-linux" ];
+      forAllVMs = f: nixpkgs.lib.genAttrs allVMs (system: f {
+        inherit system;
+        pkgs = pkgsForSystem system;
+      });
+    in
+    {
+      packages = forAllVMs ({ system, pkgs }: {
+        vm = nixos-generators.nixosGenerate {
+          system = system;
+          specialArgs = {
+            pkgs = pkgs;
+          };
+          modules = [
+            {
+              # Pin nixpkgs to the flake input, so that the packages installed
+              # come from the flake inputs.nixpkgs.url.
+              nix.registry.nixpkgs.flake = nixpkgs;
+              # set disk size to to 20G
+              virtualisation.diskSize = 20 * 1024;
+            }
+            # Apply the rest of the config.
+            ./configuration.nix
+          ];
+          format = "raw";
+        };
+      });
+    };
+}
